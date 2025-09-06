7467c462612487241d4c960c1a83571d322b4f56
Rachel Davey <rachel.davey@libfstudy.ac.uk>
2025-09-06 19:35:07 +0200

    Add static file handling and item URLs

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
-]
+] + static(settings.MEDIA_URL, docuement_root=settings.MEDIA_ROOT)
