3b0bee2b608852f676764a4d804dd6f16b423731
Rachel Davey <rachel.davey@libfstudy.ac.uk>
2025-09-06 23:04:15 +0200

    Refactor URL patterns to include core app routing

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -21,8 +21,7 @@ from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
-] + static(settings.MEDIA_URL, docuement_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
