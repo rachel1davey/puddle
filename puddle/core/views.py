41f6a3f23ce6acedc6a2daee2ce3a09bd4ca9dbc
Rachel Davey <rachel.davey@libfstudy.ac.uk>
2025-09-06 19:35:07 +0200

    Update index view to include items and categories

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from item.models import Category, Item
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(isSold=False).order_by('-created_at')[0:6]
+    categories = Category.objects.all()
+    context = {
+        'items': items,
+        'categories': categories
+    }
+    return render(request, 'core/index.html', context)
+
 
 def contact(request):
     return render(request, 'core/contact.html')
