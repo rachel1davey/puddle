0f118589166890bc5b4540c6265721040927a0ff
Rachel Davey <rachel.davey@libfstudy.ac.uk>
2025-09-10 17:29:43 +0200

    Add form for editing an item

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -25,3 +25,25 @@ class NewItemForm(forms.ModelForm):
                 'class': MASTER_CLASS
                 }),
         }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'isSold')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': MASTER_CLASS
+                }),
+            'description': forms.Textarea(attrs={
+                'class': MASTER_CLASS
+                }),
+            'price': forms.TextInput(attrs={
+                'class': MASTER_CLASS
+                }),
+            'image': forms.FileInput(attrs={
+                'class': MASTER_CLASS
+                }),
+            'isSold': forms.CheckboxInput(attrs={
+                'class': 'h-5 w-5 text-blue-600'
+                }),
+        }
